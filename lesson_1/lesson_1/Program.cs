7900e84d476a6a178578dc780854f851a672f809
schneider26 <valentina.badakwa@yandex.ru>
2021-07-21 11:56:31 +0300

    lesson 1

---
+++ lesson_1/lesson_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            string current_date = DateTime.Now.ToString("dd/MM/yyyy");
+            Console.WriteLine("Привет, " + name + ", сегодня " + current_date);
+        }
+    }
+}
